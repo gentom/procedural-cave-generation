6de39c82b8abe1cd6cc8ea76162c3945d1147748
gentom <gento.m.developer@gmail.com>
2017-08-25 15:09:00 +0900

    commit

---
+++ Assets/CellurAutomata/MapGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+
+	public int width;
+	public int height;
+
+	public string seed;
+	public bool useRandomSeed;
+
+	[Range (0, 100)]
+	public int randomFillPercent;
+
+	int[,] map;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void GenerateMap ()
+	{
+		map = new int[width, height];
+		RandomFillMap ();
+	}
+
+	void RandomFillMap ()
+	{
+		if (useRandomSeed) {
+			seed = Time.time.ToString ();
+		}
+	}
+}
